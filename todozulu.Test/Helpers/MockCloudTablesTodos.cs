bab5da28ca05c0c55c218a1626487e1bcf5077a1
GUSTAVO ADOLFO OSPINA RUIZ <gustavoospina121379@correo.itm.edu.co>
2021-08-21 08:41:31 -0500

    Unit test preparation

---
+++ todozulu.Test/Helpers/MockCloudTablesTodos.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Threading.Tasks;
+
+namespace todozulu.Test.Helpers
+{
+    public class MockCloudTablesTodos : CloudTable
+    {
+        public MockCloudTablesTodos(Uri tableAddress) : base(tableAddress)
+        {
+        }
+
+        public MockCloudTablesTodos(Uri tableAbsoluteUri, StorageCredentials credentials) : base(tableAbsoluteUri, credentials)
+        {
+        }
+
+        public MockCloudTablesTodos(StorageUri tableAddress, StorageCredentials credentials) : base(tableAddress, credentials)
+        {
+        }
+
+        public override async Task<TableResult> ExecuteAsync(TableOperation operation)
+        {
+            return await Task.FromResult(new TableResult
+            {
+                HttpStatusCode = 200,
+                Result = TestFactory.GetTodoEntity()
+            });
+        }
+    }
+}
